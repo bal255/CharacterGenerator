53546a7e7e8169d07b744762b7419edbbb34529a
bal255 <bal255@hotmail.com>
2018-04-12 10:39:20 +0200

    Initial commit

---
+++ Character.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterGenerator
+{
+    class Character
+    {
+
+        public int[] generateStats()
+        {
+
+            int dice;
+            int[] dices = new int[4];
+            int[] stats = new int[6];
+            int total = 0;
+            Random rnd = new Random();
+
+            for (int index = 0; index <= 5; index++)
+            {
+                for (int i = 0; i <= 3; i++)
+                {
+
+                    dice = rnd.Next(1, 6); // creates a number between 1 and 6
+                    dices[i] = dice;
+                    Array.Sort(dices);
+                    total = dices[1] + dices[2] + dices[3];
+
+                }
+
+                stats[index] = total;
+
+            }
+
+            return stats;
+
+        }
+
+
+    }
+
+
+
+}
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CharacterGenerator
+{
+    public partial class Form1 : Form
+    {
+
+        int[] stats = new int[6];
+
+        Character character = new Character();
+
+            
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            stats = character.generateStats();
+            for (int i = 0; i < 6; i++)
+            {
+                textBox1.AppendText(stats[i].ToString());
+            }
+
+        }
+    }
+}
